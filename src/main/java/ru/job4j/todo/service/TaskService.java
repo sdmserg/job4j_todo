4a193c55bbb1ac4bff79cdef051f181d6f8791cf
sergei <sdmserg2021@gmail.com>
2026-05-17 19:30:54 +0300

    Add interface TaskService and implementation TaskServiceImpl

---
+++ src/main/java/ru/job4j/todo/service/TaskService.java
@@ -0,0 +1,21 @@
+package ru.job4j.todo.service;
+
+import ru.job4j.todo.model.Task;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface TaskService {
+
+    Task add(Task task);
+
+    boolean update(Task task);
+
+    boolean deleteById(int id);
+
+    Optional<Task> findById(int id);
+
+    Collection<Task> findByDone(boolean done);
+
+    Collection<Task> findAll();
+}
---
+++ src/main/java/ru/job4j/todo/service/TaskServiceImpl.java
@@ -0,0 +1,46 @@
+package ru.job4j.todo.service;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.job4j.todo.model.Task;
+import ru.job4j.todo.repository.TaskStoryRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class TaskServiceImpl implements TaskService {
+    private final TaskStoryRepository taskStory;
+
+    @Override
+    public Task add(Task task) {
+        return taskStory.add(task);
+    }
+
+    @Override
+    public boolean update(Task task) {
+        return taskStory.update(task);
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return taskStory.deleteById(id);
+    }
+
+    @Override
+    public Optional<Task> findById(int id) {
+        return taskStory.findById(id);
+    }
+
+    @Override
+    public Collection<Task> findByDone(boolean done) {
+        return taskStory.findByDone(done);
+    }
+
+    @Override
+    public Collection<Task> findAll() {
+        return taskStory.findAll();
+    }
+}
