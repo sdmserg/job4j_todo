cfa974ae4dafd41585b1af7fe61fe12ba586da22
sergei <sdmserg2021@gmail.com>
2026-05-27 02:00:58 +0300

    Fixed TaskController

--- src/main/java/ru/job4j/todo/repository/TaskStore.java
+++ src/main/java/ru/job4j/todo/repository/TaskStore.java
@@ -21,141 +21,148 @@ public class TaskStore implements TaskStoreRepository {
 
     @Override
     public Task add(Task task) {
-        Transaction transaction = null;
         try (Session session = sf.openSession()) {
-            transaction = session.beginTransaction();
-            session.save(task);
-            transaction.commit();
-            return task;
-        } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.save(task);
+                transaction.commit();
+                return task;
+            } catch (HibernateException e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                LOGGER.error("Failed to add task: {}", task, e);
+                throw new IllegalStateException("Database error while saving task", e);
             }
-            LOGGER.error("Failed to add task: {}", task, e);
-            throw new IllegalStateException("Database error while saving task", e);
         }
     }
 
     @Override
     public boolean update(Task task) {
-        Transaction transaction = null;
         try (Session session = sf.openSession()) {
-            transaction = session.beginTransaction();
-            int rowsUpdated = session.createQuery(
-                    "UPDATE Task SET description = :fDescription, "
-                    + "done = :fDone, title = :fTitle WHERE id =:fId")
-                    .setParameter("fDescription", task.getDescription())
-                    .setParameter("fDone", task.isDone())
-                    .setParameter("fTitle", task.getTitle())
-                    .setParameter("fId", task.getId())
-                    .executeUpdate();
-            transaction.commit();
-            return rowsUpdated > 0;
-        } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
+            Transaction transaction = session.beginTransaction();
+            try {
+                int rowsUpdated = session.createQuery(
+                                "UPDATE Task SET description = :fDescription, "
+                                        + "done = :fDone, title = :fTitle WHERE id =:fId")
+                        .setParameter("fDescription", task.getDescription())
+                        .setParameter("fDone", task.isDone())
+                        .setParameter("fTitle", task.getTitle())
+                        .setParameter("fId", task.getId())
+                        .executeUpdate();
+                transaction.commit();
+                return rowsUpdated > 0;
+            } catch (HibernateException e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                LOGGER.error("Failed to update task: {}", task, e);
+                throw new IllegalStateException("Database error while updating task", e);
             }
-            LOGGER.error("Failed to update task: {}", task, e);
-            throw new IllegalStateException("Database error while updating task", e);
         }
     }
 
     @Override
     public boolean completeTask(int id) {
-        Transaction transaction = null;
         try (Session session = sf.openSession()) {
-            transaction = session.beginTransaction();
-            int rowsUpdate = session.createQuery(
-                    "UPDATE Task SET done = true WHERE id = :fId")
-                    .setParameter("fId", id)
-                    .executeUpdate();
-            transaction.commit();
-            return rowsUpdate > 0;
-        } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
+            Transaction transaction = session.beginTransaction();
+            try {
+                int rowsUpdate = session.createQuery(
+                                "UPDATE Task SET done = true WHERE id = :fId")
+                        .setParameter("fId", id)
+                        .executeUpdate();
+                transaction.commit();
+                return rowsUpdate > 0;
+            } catch (HibernateException e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                LOGGER.error("Failed to complete task with id: {}", id, e);
+                throw new IllegalStateException("Database error while completing task", e);
             }
-            LOGGER.error("Failed to complete task with id: {}", id, e);
-            throw new IllegalStateException("Database error while completing task", e);
         }
     }
 
     @Override
     public boolean deleteById(int id) {
-        Transaction transaction = null;
         try (Session session = sf.openSession()) {
-            transaction = session.beginTransaction();
-            int rowsDeleted = session.createQuery(
-                            "DELETE FROM Task WHERE id =:fId")
-                    .setParameter("fId", id)
-                    .executeUpdate();
-            transaction.commit();
-            return rowsDeleted > 0;
-        } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
+            Transaction transaction = session.beginTransaction();
+            try {
+                int rowsDeleted = session.createQuery(
+                                "DELETE FROM Task WHERE id =:fId")
+                        .setParameter("fId", id)
+                        .executeUpdate();
+                transaction.commit();
+                return rowsDeleted > 0;
+            } catch (HibernateException e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                LOGGER.error("Failed to delete task with id: {}", id, e);
+                throw new IllegalStateException("Database error while deleting task", e);
             }
-            LOGGER.error("Failed to delete task with id: {}", id, e);
-            throw new IllegalStateException("Database error while deleting task", e);
         }
     }
 
     @Override
     public Optional<Task> findById(int id) {
-        Transaction transaction = null;
         try (Session session = sf.openSession()) {
-            transaction = session.beginTransaction();
-            var task = session.createQuery(
-                    "FROM Task WHERE id = :fId", Task.class)
-                    .setParameter("fId", id)
-                    .uniqueResultOptional();
-            transaction.commit();
-            return task;
-        } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
+            Transaction transaction = session.beginTransaction();
+            try {
+                var task = session.createQuery(
+                                "FROM Task WHERE id = :fId", Task.class)
+                        .setParameter("fId", id)
+                        .uniqueResultOptional();
+                transaction.commit();
+                return task;
+            } catch (HibernateException e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                LOGGER.error("Failed to find task with id: {}", id, e);
+                throw new IllegalStateException("Database error while finding task", e);
             }
-            LOGGER.error("Failed to find task with id: {}", id, e);
-            throw new IllegalStateException("Database error while finding task", e);
         }
     }
 
     @Override
     public Collection<Task> findByDone(boolean done) {
-        Transaction transaction = null;
         try (Session session = sf.openSession()) {
-            transaction = session.beginTransaction();
-            var task = session.createQuery(
-                            "FROM Task WHERE done = :fDone", Task.class)
-                    .setParameter("fDone", done)
-                    .list();
-            transaction.commit();
-            return task;
-        } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
+            Transaction transaction = session.beginTransaction();
+            try {
+                var task = session.createQuery(
+                                "FROM Task WHERE done = :fDone", Task.class)
+                        .setParameter("fDone", done)
+                        .list();
+                transaction.commit();
+                return task;
+            } catch (HibernateException e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                LOGGER.error("Failed to find tasks", e);
+                throw new IllegalStateException("Database error while finding tasks", e);
             }
-            LOGGER.error("Failed to find tasks", e);
-            throw new IllegalStateException("Database error while finding tasks", e);
         }
     }
 
     @Override
     public Collection<Task> findAll() {
-        Transaction transaction = null;
         try (Session session = sf.openSession()) {
-            transaction = session.beginTransaction();
-            var task = session.createQuery(
-                            "FROM Task", Task.class)
-                    .list();
-            transaction.commit();
-            return task;
-        } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
+            Transaction transaction = session.beginTransaction();
+            try {
+                var task = session.createQuery(
+                                "FROM Task", Task.class)
+                        .list();
+                transaction.commit();
+                return task;
+            } catch (HibernateException e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                LOGGER.error("Failed to find tasks", e);
+                throw new IllegalStateException("Database error while finding tasks", e);
             }
-            LOGGER.error("Failed to find tasks", e);
-            throw new IllegalStateException("Database error while finding tasks", e);
         }
     }
 }
